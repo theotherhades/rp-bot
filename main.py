63b4c1f366afadcb71b406ac700e9ea29e98bcfd
theotherhades <64885838+theotherhades@users.noreply.github.com>
2023-01-15 18:23:05 +1300

    stuff

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import os
 import nextcord
-from nextcord import Interaction
+from nextcord import Interaction, SlashOption
 from nextcord.ext import commands
+from pymongo import MongoClient
 
 client = commands.Bot()
+
+cluster = MongoClient(os.environ["DB_URL"])
+db = cluster["test"]
 
 GUILD_IDS = list()
 for guild in client.guilds:
@@ -16,5 +20,33 @@ async def on_ready():
 @client.slash_command(name = "hi", description = "says hi", guild_ids = GUILD_IDS)
 async def hi(interaction: Interaction):
     await interaction.response.send_message(f"hi {interaction.user.name}")
+
+@client.slash_command(name = "join", description = "", guild_ids = GUILD_IDS)
+async def join(interaction: Interaction, name: str = SlashOption(description = "The name of your nation. This can always be changed later.")):
+    col = db["users"]
+    col.insert_one({
+        "_id": interaction.user.id,
+        "name": name,
+        "treasury": 100000,
+        "population": 1000,
+        "stability": 10,
+        "resources": {
+            "steel": 0,
+            "oil": 0,
+            "wood": 0,
+            "precious_metals": 0
+        },
+        "military": {
+            "troops": 0,
+            "cavalry": 0,
+            "artillery": 0,
+            "boats": 0,
+            "aircraft": 0
+        }
+    })
+    embed = nextcord.Embed(title = "Success!", description = f"You've successfully joined the game as **{name}**!")
+    await interaction.response.send_message(embed = embed)
+
+# do something like this for money later https://stackoverflow.com/questions/63625246/discord-py-bot-run-function-at-specific-time-every-day
 
 client.run(os.environ["CLIENT_TOKEN"])
